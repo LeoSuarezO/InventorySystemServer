ea2932cf5b51d510bf7c690e6f52287dc7dba04d
LeoSuarezO <leonardo901.ls2@gmail.com>
2024-02-09 17:00:52 -0500

    First commit

---
+++ src/test/java/edu/inventory/core/InventorySystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.inventory.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
